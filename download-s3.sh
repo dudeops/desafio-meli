9db47daecc5d4cfb52a790f273bbb5148fe96514
Rodrigo Ramalho - Dude <thedigu@gmail.com>
2022-05-02 17:26:31 -0300

    Update download-s3.sh

--- download-s3.sh
+++ download-s3.sh
@@ -1,20 +1,22 @@
 #!/bin/sh
 
 read -p "Informe o nome completo do arquivo (com o .extensão) " file
+read -p "Informe o nome do bucket (por exemplo s3://Desafio-meli/nome do bucket/)" bucket
 
-# Habilita log copiando a saída padrão para o arquivo LOGFILE
+# Arquivo de saída para o log
 LOGFILE="./log/desafio-meli.log"
-        #exec > >(tee -ia $LOGFILE)
-# Caso haja erro, segue para a saída de ERROS
-        #exec 2> >(tee -ia $LOGFILE >&2)
 
 #Valida se o arquivo existe
-if [ -f "$file" ]
+    if [ -f "$file" ]
+
+#Se o arquvio existir, continuamos
 then
     echo "Encontrei o seu arquivo ;]"
-    echo "Iniciando a copia do arquivo para o bucket =] "
-    aws s3 cp s3://desafio-meli/arquivos-utilitarios/ ./$file 
+    echo "Iniciando a copia do arquivo para o bucket, logo em seguida você poderá vizualizar em sua estação =] "
+        aws s3 sync /servidor Fury s3://desafio-meli/$bucket/ 
+        aws s3 cp s3://desafio-meli/$bucket/ ./$file
+          
     echo "Download do arquivo " $file " executado com sucesso " $(date) >> $LOGFILE
 else
-        echo "Não consegui encontrar o arquivo " $file " =´[  " $(date) >> $LOGFILE
+    echo "Não consegui encontrar o arquivo " $file " =´[ " $(date) >> $LOGFILE
 fi
