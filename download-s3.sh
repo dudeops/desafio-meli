8012259d64427815d6ceebfb17cbdd09b5f31cde
Rodrigo Ramalho - Dude <thedigu@gmail.com>
2022-05-09 17:52:24 -0300

    Update download-s3.sh

--- download-s3.sh
+++ download-s3.sh
@@ -13,10 +13,12 @@ LOGFILE="./log/desafio-meli.log"
 then
     echo "Encontrei o seu arquivo ;]"
     echo "Iniciando a copia do arquivo para o bucket, logo em seguida você poderá vizualizar em sua estação =] "
-        aws s3 sync /servidor Fury s3://desafio-meli/$bucket/ 
+        
         aws s3 cp s3://desafio-meli/$bucket/ ./$file
           
     echo "Download do arquivo " $file " executado com sucesso " $(date) >> $LOGFILE
+
+#Se o arquvio não existir, exibe a mensagem abaixo
 else
     echo "Não consegui encontrar o arquivo " $file " =´[ " $(date) >> $LOGFILE
 fi
